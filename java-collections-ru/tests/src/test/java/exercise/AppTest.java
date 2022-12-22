e098df36e19a44914c4684525c7d706f44c99c69
@hexlet/cli <support@hexlet.io>
2022-12-22 16:13:18 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -13,6 +13,10 @@ class AppTest {
     @Test
     void testTake() {
         // BEGIN
+        Integer[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int testNum = 5;
+        int testNum2 = 6;
+
         List<Integer> list1 = new ArrayList<>(List.of(1, 2, 3, 4));
         int n1 = 2;
         List<Integer> expected1 = new ArrayList<>(List.of(1, 2));
@@ -20,8 +24,13 @@ class AppTest {
         int n2 = 8;
         List<Integer> expected2 = new ArrayList<>(List.of(7, 3, 10));
 
-        assertThat(App.take(list1, n1)).isEqualTo(expected1);
+        Assertions.assertEquals(App.take(list1, n1), expected1);
         assertThat(App.take(list2, n2)).isEqualTo(expected2);
+
+        List<Integer> result = (App.take(Arrays.asList(testArray), testNum));
+        int resultSize = result.size();
+
+        assertThat(resultSize).isEqualTo(testNum);
         // END
     }
 }
