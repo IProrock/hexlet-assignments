1941a21064a1807296ef2e7373450adc941bfa1b
@hexlet/cli <support@hexlet.io>
2022-12-22 15:31:21 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -13,15 +13,15 @@ class AppTest {
     @Test
     void testTake() {
         // BEGIN
-//        List<Integer> list1 = new ArrayList<>(List.of(1, 2, 3, 4));
-//        int n1 = 2;
-//        List<Integer> expected1 = new ArrayList<>(List.of(1, 2));
-//        List<Integer> list2 = new ArrayList<>(List.of(7, 3, 10));
-//        int n2 = 8;
-//        List<Integer> expected2 = new ArrayList<>(List.of(7, 3, 10));
+        List<Integer> list1 = new ArrayList<>(List.of(1, 2, 3, 4));
+        int n1 = 2;
+        List<Integer> expected1 = new ArrayList<>(List.of(1, 2));
+        List<Integer> list2 = new ArrayList<>(List.of(7, 3, 10));
+        int n2 = 8;
+        List<Integer> expected2 = new ArrayList<>(List.of(7, 3, 10));
 
-//        assertThat(App.take(list1, n1)).isEqualTo(expected1);
-//        assertThat(App.take(list2, n2)).isEqualTo(expected2);
+        assertThat(App.take(list1, n1)).isEqualTo(expected1);
+        assertThat(App.take(list2, n2)).isEqualTo(expected2);
         // END
     }
 }
