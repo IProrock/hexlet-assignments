dd3089a452cb05700ffffe28a633b7ee7eb010d3
@hexlet/cli <support@hexlet.io>
2023-09-13 20:59:35 +0300

    submit java-spring-ru/spring-lifecycle

--- java-spring-ru/spring-lifecycle/src/main/java/exercise/CustomBeanPostProcessor.java
+++ java-spring-ru/spring-lifecycle/src/main/java/exercise/CustomBeanPostProcessor.java
@@ -4,6 +4,24 @@ package exercise;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-// BEGIN
+import org.springframework.stereotype.Component;
 
+// BEGIN
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName)
+            throws BeansException {
+        System.out.println("Called postPrBEFOREInitialization for bean: " + beanName);
+
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Called postPrAFTERInitialization for bean: " + beanName);
+
+        return bean;
+    }
+}
 // END
